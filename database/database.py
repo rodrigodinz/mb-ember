4aee1b5efadd7db8d775b02965e7a67fa620d326
Rodrigo <rodrigodinizsotero10@gmail.com>
2026-08-14 13:27:27 -0300

    DEPOIS DE MUITO TEMPO, commit com todos os relationships atualizados, def __init__ apagados, seed.py atualizada e funcionando, criando o banco de dados normalmente e inserindo os valores corretamente, assim como a primeira venda registrada

--- database/database.py
+++ database/database.py
@@ -13,7 +13,7 @@ from sqlalchemy import (
     JSON
 )
 
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.orm import declarative_base
 
 from datetime import datetime
@@ -37,20 +37,18 @@ class Usuario(Base):
     perfil = Column(
         "perfil_usuario", Enum("vendedor", "usuario", "admin", name="perfil_usuario")
     )
-    ativo = Column("ativo_usuario", Boolean)
+    ativo = Column("ativo_usuario", Boolean, default=True)
     data_cadastro = Column("data_cadastro_usuario", DateTime)
 
-    def __init__(self, nome, email, senha_hash, perfil, ativo=True):
-        self.nome = nome
-        self.email = email
-        self.senha_hash = senha_hash
-        self.perfil = perfil
-        self.ativo = ativo
+    rel_log_acesso = relationship("Log_Acesso", back_populates="rel_usuario")
+    rel_mov_estoque = relationship("Movimentacao_estoque", back_populates="rel_usuario")
+    rel_venda = relationship("Venda", back_populates="rel_usuario")
+    rel_relatorio = relationship("Relatorio", back_populates="rel_usuario")
+    rel_abastecimento = relationship("Abastecimento", back_populates="rel_usuario")
 
     # autenticar(email, senha) - boolean
     # alterarSenha(novaSenha) - Void
     # obterMeusDados () - Usuario
-
 
 class Log_Acesso(Base):
     __tablename__ = "Log de acesso"
@@ -59,16 +57,11 @@ class Log_Acesso(Base):
     id_usuario = Column("id_usuario", Integer, ForeignKey("Usuarios.id_usuario"))
     data_hora = Column("datahora_log", DateTime)
     ip = Column("ip_log", VARCHAR)
-    sucesso = Column("sucesso_log", Boolean)
+    sucesso = Column("sucesso_log", Boolean, default=True)
 
-    def __init__(self, id_usuario, data_hora, ip, sucesso):
-        self.id_usuario = id_usuario
-        self.data_hora = data_hora
-        self.ip = ip
-        self.sucesso = sucesso
+    rel_usuario = relationship("Usuario", back_populates="rel_log_acesso")
 
     # registrarAcesso (sucesso, ip) - void
-
 
 class Churrasqueira(Base):
     __tablename__ = "Churrasqueira"
@@ -80,35 +73,33 @@ class Churrasqueira(Base):
     modelo = Column("modelo_churrasqueira", VARCHAR)
     descricao = Column("descricao_churrasqueira", VARCHAR)
     preco_venda = Column("preco_venda_churrasqueira", DECIMAL)
-    ativo = Column("ativo_churrasqueira", Boolean)
-
-    def __init__(self, codigo, modelo, descricao, preco_venda, ativo=True):
-        self.codigo = codigo
-        self.modelo = modelo
-        self.descricao = descricao
-        self.preco_venda = preco_venda
-        self.ativo = ativo
+    ativo = Column("ativo_churrasqueira", Boolean, default=True)
 
     # cadastrar() - Void
     # editar() - Void
     # pesquisar(termo) - list<Churrasqueira>
 
+    rel_estoque = relationship("Estoque", back_populates="rel_churrasqueira", uselist=False)
+
+    rel_mov_estoque = relationship("Movimentacao_estoque", back_populates="rel_churrasqueira")
+
+    rel_item_venda = relationship("Item_venda", back_populates="rel_churrasqueira")
+
+    rel_item_abastecimento = relationship("Item_abastecimento", back_populates="rel_churrasqueira")
 
 class Estoque(Base):
     __tablename__ = "Estoque"
 
     id_estoque = Column("id_estoque", Integer, primary_key=True, autoincrement=True)
-    id_churrasqueira = Column("id_churrasqueira", Integer, ForeignKey("Churrasqueira.id_churrasqueira"))
+    id_churrasqueira = Column("id_churrasqueira", Integer, ForeignKey("Churrasqueira.id_churrasqueira"), unique=True)
     quantidade_disponivel = Column("quantidade_disponivel", Integer)
     data_atualizacao = Column("data_atualizacao_estoque", DateTime, default=datetime.now, onupdate=datetime.now)
-
-    def __init__(self, id_churrasqueira, quantidade_disponivel):
-        self.id_churrasqueira = id_churrasqueira
-        self.quantidade_disponivel = quantidade_disponivel
 
     # obterSaldo() - Int
     # atualizarQuantidade() - void
     # verificarDisponibilidade(qtd) - boolean
+
+    rel_churrasqueira = relationship("Churrasqueira", back_populates="rel_estoque")
 
 class Abastecimento(Base):
     __tablename__ = "Abastecimento_Estoque"
@@ -121,31 +112,23 @@ class Abastecimento(Base):
     data_cancelamento = Column("data_cancelamento", DateTime)
     motivo_cancelamento = Column("motivo_cancelamento", VARCHAR)
 
-    def __init__(self, id_abastecimento, id_usuario, data_abastecimento, observacoes, cancelado, data_cancelamento, motivo_cancelamento):
-        self.id_abastecimento = id_abastecimento
-        self.id_usuario = id_usuario
-        self.data_abastecimento = data_abastecimento
-        self.observacoes = observacoes
-        self.cancelado = cancelado
-        self.data_cancelamento = data_cancelamento
-        self.motivo_cancelamento = motivo_cancelamento
+    rel_item_abastecimento = relationship("Item_abastecimento", back_populates="rel_abastecimento")
+    rel_usuario = relationship("Usuario", back_populates="rel_abastecimento")
 
     # cadastrar() - Void
     # cancelar(motivo) - Void
     # listar(filtros) - list<abastecimentos>
 
 class Item_abastecimento(Base):
-    __tablename__ = "qual_item_abastecimento"
+    __tablename__ = "Item_Abastecimento"
 
     id_item_abastecimento = Column("id_item_abastecimento", Integer, primary_key=True, autoincrement=True)
     id_abastecimento = Column("id_abastecimento", Integer, ForeignKey("Abastecimento_Estoque.id_abastecimento"))
     id_churrasqueira = Column("id_churrasqueira", Integer, ForeignKey("Churrasqueira.id_churrasqueira"))
     quantidade = Column("quantidade_abastecimento", Integer)
 
-    def __init__(self, id_abastecimento, id_churrasqueira, quantidade):
-        self.id_abastecimento = id_abastecimento
-        self.id_churrasqueira = id_churrasqueira
-        self.quantidade = quantidade
+    rel_abastecimento = relationship("Abastecimento", back_populates="rel_item_abastecimento")
+    rel_churrasqueira = relationship("Churrasqueira", back_populates="rel_item_abastecimento")
 
     # adicionarItem() - void
     # removerItem() - void
@@ -164,19 +147,11 @@ class Movimentacao_estoque(Base):
     saldo_posterior = Column("saldo_posterior", Integer)
     observacoes = Column("observacoes_movimentacao", VARCHAR)
 
-    def __init__(self, id_usuario, id_churrasqueira, data_hora, tipo_movimentacao, origem, quantidade, saldo_anterior, saldo_posterior, observacoes):
-        self.id_usuario = id_usuario
-        self.id_churrasqueira = id_churrasqueira
-        self.data_hora = data_hora
-        self.tipo_movimentacao = tipo_movimentacao
-        self.origem = origem
-        self.quantidade = quantidade
-        self.saldo_anterior = saldo_anterior
-        self.saldo_posterior = saldo_posterior
-        self.observacoes = observacoes
-
     # registrar() - Void
     # listar(filtros) - list<Movimentacao_estoque>
+
+    rel_churrasqueira = relationship("Churrasqueira", back_populates="rel_mov_estoque")
+    rel_usuario = relationship("Usuario", back_populates="rel_mov_estoque")
 
     # O que fazer com o tipo_movimentacao (ENUM) (Entrada e saída)
     # O que fazer com a origem (ENUM) (Abastecimento, venda, cancelamento_venda, ajuste_manual)
@@ -185,62 +160,52 @@ class Cliente(Base):
     __tablename__ = "Clientes"
 
     id_cliente = Column("id_cliente", Integer, primary_key=True, autoincrement=True)
-    tipo = Column("tipo_cliente", Enum("fisica", "juridica", name="tipo_cliente"))
     nome = Column("nome_cliente", VARCHAR)
-    cpf_cnjp = Column("cpf_cnpj_cliente", VARCHAR)
+    cpf_cnpj = Column("cpf_cnpj_cliente", VARCHAR)
     telefone = Column("telefone_cliente", VARCHAR)
     email = Column("email_cliente", VARCHAR)
     endereco = Column("endereco_cliente", VARCHAR)
     cidade = Column("cidade_cliente", VARCHAR)
     estado = Column("estado_cliente", VARCHAR)
     cep = Column("cep_cliente", VARCHAR)
-    ativo = Column("ativo_cliente", Boolean)
+    ativo = Column("ativo_cliente", Boolean, default=True)
     data_cadastro = Column("data_cadastro_cliente", DateTime)
+    tipo_cliente = Column("tipo_cliente", String)
 
-    def __init__(self, tipo, nome, cpf_cnpj, telefone, email, endereco, cidade, estado, cep, data_cadastro, ativo=True):
-        self.tipo = tipo
-        self.nome = nome
-        self.cpf_cnpj = cpf_cnpj
-        self.telefone = telefone
-        self.email = email
-        self.endereco = endereco
-        self.cidade = cidade
-        self.estado = estado
-        self.cep = cep
-        self.ativo = ativo
-        self.data_cadastro = data_cadastro
+    __mapper_args__ = {
+        "polymorphic_on": tipo_cliente,
+        "polymorphic_identity": "cliente"
+    }
+
+    rel_venda = relationship("Venda", back_populates="rel_cliente")
 
     # cadastrar() - Void
     # editar() - Void
     # desativar() - Void
 
-class Loja(Base):
+class Loja(Cliente):
     __tablename__ = "Lojas"
 
-    id_loja = Column("id_loja", Integer, primary_key=True, autoincrement=True)
-    id_cliente = Column("id_cliente", Integer, ForeignKey("Clientes.id_cliente"), unique=True)
+    id_cliente = Column("id_cliente", Integer, ForeignKey("Clientes.id_cliente"), unique=True, primary_key=True)
     razao_social = Column("razao_social_loja", VARCHAR)
     inscricao_estadual = Column("inscricao_estadual_loja", VARCHAR)
 
-    def __init__(self, id_cliente, razao_social, inscricao_estadual):
-        self.id_cliente = id_cliente
-        self.razao_social = razao_social
-        self.inscricao_estadual = inscricao_estadual
+    __mapper_args__ = {
+        "polymorphic_identity": "loja"
+    }
 
     # obterDadosLoja() - Loja
 
-class Consumidor_Final(Base):
+class Consumidor_Final(Cliente):
     __tablename__ = "Consumidor_Final"
 
-    id_consumidor = Column("id_consumidor", Integer, primary_key=True, autoincrement=True)
-    id_cliente = Column("id_cliente", Integer, ForeignKey("Clientes.id_cliente"))
+    id_cliente = Column("id_cliente", Integer, ForeignKey("Clientes.id_cliente"), unique=True, primary_key=True)
     data_nascimento = Column("data_nascimento_consumidor", Date)
     observacoes = Column("observacoes_consumidor", VARCHAR)
 
-    def __init__(self, id_cliente, data_nascimento, observacoes):
-        self.id_cliente = id_cliente
-        self.data_nascimento = data_nascimento
-        self.observacoes = observacoes
+    __mapper_args__ = {
+        "polymorphic_identity": "consumidor_final"
+    }
     
     # obterDadosConsumidor() - Consumidor_Final
 
@@ -258,16 +223,10 @@ class Venda(Base):
     data_cancelamento = Column("data_cancelamento_venda", DateTime)
     motivo_cancelamento = Column("motivo_cancelamento_venda", VARCHAR)
 
-    def __init__(self, id_usuario, id_cliente, data_venda, forma_pagamento, valor_total, observacoes, status, data_cancelamento, motivo_cancelamento):
-        self.id_usuario = id_usuario
-        self.id_cliente = id_cliente
-        self.data_venda = data_venda
-        self.forma_pagamento = forma_pagamento
-        self.valor_total = valor_total
-        self.observacoes = observacoes
-        self.status = status
-        self.data_cancelamento = data_cancelamento
-        self.motivo_cancelamento = motivo_cancelamento
+    rel_usuario = relationship("Usuario", back_populates="rel_venda")
+    rel_cliente = relationship("Cliente", back_populates="rel_venda")
+    rel_item_venda = relationship("Item_venda", back_populates="rel_venda", cascade="all, delete-orphan")
+    rel_forma_pagamento = relationship("Forma_pagamento", back_populates="rel_venda")
 
     # registrar() - Void
     # cancelar() - Void
@@ -283,12 +242,9 @@ class Item_venda(Base):
     preco_unitario = Column("preco_unitario_item_venda", DECIMAL)
     subtotal = Column("subtotal_item_venda", DECIMAL)
 
-    def __init__(self, id_venda, id_churrasqueira, quantidade, preco_unitario, subtotal):
-        self.id_venda = id_venda
-        self.id_churrasqueira = id_churrasqueira
-        self.quantidade = quantidade
-        self.preco_unitario = preco_unitario
-        self.subtotal = subtotal
+    rel_venda = relationship("Venda", back_populates="rel_item_venda")
+
+    rel_churrasqueira = relationship("Churrasqueira", back_populates="rel_item_venda")
 
     #CalcularSubTotal() - DECIMAL
 
@@ -297,11 +253,9 @@ class Forma_pagamento(Base):
 
     id_forma_pagamento = Column("id_forma_pagamento", Integer, primary_key=True, autoincrement=True)
     nome = Column("nome_forma_pagamento", VARCHAR, unique=True)
-    ativo = Column("ativo_forma_pagamento", Boolean)
+    ativo = Column("ativo_forma_pagamento", Boolean, default=True)
 
-    def __init__(self, nome, ativo=True):
-        self.nome = nome
-        self.ativo = ativo
+    rel_venda = relationship("Venda", back_populates="rel_forma_pagamento")
 
     # cadastrar() - Void
     # editar() - Void
@@ -316,11 +270,7 @@ class Relatorio(Base):
     filtros_aplicados = Column("filtros_aplicados_relatorios", JSON)
     data_geracao = Column("data_relatorio", DateTime)
 
-    def __init__(self, id_usuario, tipo_relatorio, filtros_aplicados, data_geracao):
-        self.id_usuario = id_usuario
-        self.tipo_relatorio = tipo_relatorio
-        self.filtros_aplicados = filtros_aplicados
-        self.data_geracao = data_geracao
+    rel_usuario = relationship("Usuario", back_populates="rel_relatorio")
 
     # gerar() - Void
     # exportarPDF() - File
--- database/seed.py
+++ database/seed.py
@@ -1,9 +1,14 @@
+import datetime
+
 from database import Session
 from database import(
     Usuario,
     Forma_pagamento,
     Churrasqueira,
-    Estoque
+    Estoque,
+    Venda,
+    Item_venda,
+    Consumidor_Final
 )
 
 with Session() as session:
@@ -28,8 +33,8 @@ with Session() as session:
 
     teste_churrasqueira = Churrasqueira(
         codigo="CHIN01",
-        modelo="Inox 01",
-        descricao="Churrasqueira Inox Mundial 01",
+        modelo="Mundial Básica",
+        descricao="Churrasqueira Inox Mundial Básica",
         preco_venda=200.00
     )
 
@@ -37,12 +42,88 @@ with Session() as session:
     session.flush()
 
     teste_estoque = Estoque(
-        id_churrasqueira=teste_churrasqueira.id_churrasqueira,
-        quantidade_disponivel=20,
+        rel_churrasqueira=teste_churrasqueira,
+        quantidade_disponivel=10,
     )
 
     session.add(teste_estoque)
+
+    ##--------------------------------- PÓS RELATIONSHIP ---------------------------------##
+
+    nova_churrasqueira = [
+        Churrasqueira(
+            codigo="CHIN02",
+            modelo="Mundial Luxo",
+            descricao="Churrasqueira Inox Mundial de luxo",
+            preco_venda=250.00
+            ),
+        Churrasqueira( 
+            codigo="CHIN03",
+            modelo="Mundial Premium",
+            descricao="Churrasqueira Inox Mundial Premium",
+            preco_venda=300.00
+        ),
+        Churrasqueira(
+            codigo="CHIN04",
+            modelo="Mundial Master",
+            descricao="Churrasqueira Inox Mundial Master",
+            preco_venda=350.00
+        )
+    ]
+
+    session.add_all(nova_churrasqueira)
+
+    cliente_teste = Consumidor_Final(
+        nome="Rodrigo Teste",
+        cpf_cnpj="123.456.789-00",
+        telefone="(11) 94444-4464",
+        email="email_teste@gmail.com",
+        endereco="Rua Teste, 123",
+        cidade="Cidade Teste",
+        estado="ET",
+        cep="12345-678",
+        data_cadastro=datetime.datetime.now(),
+        data_nascimento=datetime.datetime.now().date(),
+        observacoes="Cliente de teste para vendas",
+    )
+
+    churras_inox_basica = teste_churrasqueira
+
+    churras_inox_luxo = nova_churrasqueira[0]
+
+    venda_atual = Venda(
+        data_venda=datetime.datetime.now(),
+        valor_total=churras_inox_basica.preco_venda + churras_inox_luxo.preco_venda,
+        status="concluida",
+        rel_forma_pagamento=formas_pagamento[0],
+        rel_usuario=teste_usuario,
+        rel_cliente=cliente_teste,
+        rel_item_venda=[
+            Item_venda(
+                rel_churrasqueira=churras_inox_basica,
+                quantidade=1,
+                preco_unitario=churras_inox_basica.preco_venda,
+                subtotal=churras_inox_basica.preco_venda,
+
+
+            ),
+            Item_venda(
+                rel_churrasqueira=churras_inox_luxo,
+                quantidade=1,
+                preco_unitario=churras_inox_luxo.preco_venda,
+                subtotal=churras_inox_luxo.preco_venda,
+            )
+        ]
+    )
+
+    session.add(venda_atual)
     session.commit()
+
+    venda_salva = session.query(Venda).first()
+    print(f"Cliente: {venda_salva.rel_cliente.nome} \nItens comprados:")
+
+    for item in venda_salva.rel_item_venda:
+        print(f"\n{item.rel_churrasqueira.modelo} \n- Quantidade:  {item.quantidade:.2f} \n- Preço Unitário: {item.preco_unitario:.2f} \n- Subtotal: {item.subtotal:.2f}")
 
 
 
